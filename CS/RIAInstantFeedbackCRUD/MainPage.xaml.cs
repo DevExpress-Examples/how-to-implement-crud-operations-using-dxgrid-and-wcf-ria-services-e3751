86caf9e5f39f39515845bbe1f27cf0a8761be904
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RIAInstantFeedbackCRUD/MainPage.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.DomainServices.Client;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Grid;
+using RIAInstantFeedbackCRUD.Web;
+
+namespace RIAInstantFeedbackCRUD {
+    public partial class MainPage : UserControl {
+        NorthwindContext context;
+        Control control;
+        Customers newCustomer;
+        Customers customerToEdit;
+        Dispatcher uiDispatcher;
+
+        public MainPage() {
+            InitializeComponent();
+            uiDispatcher = grid.Dispatcher;
+            context = new NorthwindContext();
+            riaInstantSource.DomainContext = context;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e) {
+            newCustomer = CreateNewCustomer();
+            EditCustomer(newCustomer, "Add new customer", CloseAddNewCustomerHandler);
+        }
+        private void Remove_Click(object sender, RoutedEventArgs e) {
+            DeleteSelectedCustomer(view.FocusedRowHandle);
+        }
+        private void Edit_Click(object sender, RoutedEventArgs e) {
+            EditSelectedCustomer(view.FocusedRowHandle);
+        }
+        private void view_RowDoubleClick(object sender, RowDoubleClickEventArgs e) {
+            EditSelectedCustomer(e.HitInfo.RowHandle);
+        }
+        private void view_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+            if(e.Key == System.Windows.Input.Key.Delete) {
+                DeleteSelectedCustomer(view.FocusedRowHandle);
+            }
+            if(e.Key == System.Windows.Input.Key.Enter) {
+                EditSelectedCustomer(view.FocusedRowHandle);
+            }
+        }
+
+        string GetCustomerIDByRowHandle(int rowHandle) {
+            return (string)grid.GetCellValue(rowHandle, colCustomerID);
+        }
+        void FindCustomerByIDAndProcess(string customerID, Action<Customers> action) {
+            try {
+                context.Load<Customers>(context.GetCustomerByIDQuery(customerID), FindCustomerByIDCallback, action);
+            } catch(Exception ex) {
+                HandleException(ex);
+            }
+        }
+        void FindCustomerByIDCallback(LoadOperation<Customers> loadOperation) {
+            if(loadOperation.HasError) {
+                if(loadOperation.CanCancel) loadOperation.Cancel();
+                HandleException(loadOperation.Error);
+                loadOperation.MarkErrorAsHandled();
+                return;
+            }
+            Action<Customers> action = (Action<Customers>)loadOperation.UserState;
+            foreach(Customers customer in loadOperation.Entities) {
+                try {
+                    uiDispatcher.BeginInvoke(() => action(customer));
+                } catch(Exception ex) {
+                    HandleException(ex);
+                }
+            }
+        }
+
+        Customers CreateNewCustomer() {
+            Customers newCustomer = new Customers();
+            newCustomer.CustomerID = GenerateCustomerID();
+            return newCustomer;
+        }
+        string GenerateCustomerID() {
+            const int IDLength = 5;
+            string result = String.Empty;
+            Random rnd = new Random();
+            for(int i = 0; i < IDLength; i++) {
+                result += Convert.ToChar(rnd.Next(65, 90));
+            }
+            return result;
+        }
+        void DeleteSelectedCustomer(int rowHandle) {
+            if(rowHandle < 0) return;
+            if(MessageBox.Show("Do you really want to delete the selected customer?", "Delete Customer", MessageBoxButton.OKCancel) != MessageBoxResult.OK) return;
+            FindCustomerByIDAndProcess(GetCustomerIDByRowHandle(rowHandle), customer => { context.Customers.Remove(customer); SaveChandes(); });
+        }
+        void EditSelectedCustomer(int rowHandle) {
+            if(rowHandle < 0) return;
+            FindCustomerByIDAndProcess(GetCustomerIDByRowHandle(rowHandle), customer => {
+                customerToEdit = customer; EditCustomer(customerToEdit, "Edit customer", CloseEditCustomerHandler);
+            });
+        }
+        void EditCustomer(Customers customer, string windowTitle, EventHandler closedDelegate) {
+            control = new ContentControl { Template = (ControlTemplate)Resources["EditRecordTemplate"] };
+            control.DataContext = customer;
+
+            DXDialog dialog = new DXDialog(windowTitle, DialogButtons.OkCancel);
+            dialog.Content = control;
+            dialog.Closed += closedDelegate;
+            dialog.ShowDialog();
+        }
+        void CloseAddNewCustomerHandler(object sender, EventArgs e) {
+            if(((DXDialog)sender).DialogResult == DialogResult.OK) {
+                context.Customers.Add(newCustomer);
+                SaveChandes();
+            }
+            control = null;
+            newCustomer = null;
+        }
+        void CloseEditCustomerHandler(object sender, EventArgs e) {
+            if(((DXDialog)sender).DialogResult == DialogResult.OK) {
+                SaveChandes();
+            }
+            control = null;
+            customerToEdit = null;
+        }
+
+        void SaveChandes() {
+            try {
+                context.SubmitChanges(SubmitChangesCallback, null);
+            } catch(Exception ex) {
+                context.RejectChanges();
+                HandleException(ex);
+            }
+        }
+        void SubmitChangesCallback(SubmitOperation submitOperation) {
+            if(submitOperation.HasError) {
+                if(submitOperation.CanCancel) submitOperation.Cancel();
+                context.RejectChanges();
+                HandleException(submitOperation.Error);
+                submitOperation.MarkErrorAsHandled();
+            }
+            uiDispatcher.BeginInvoke(() => riaInstantSource.Refresh());
+        }
+        void HandleException(Exception ex) {
+            uiDispatcher.BeginInvoke(() => MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK));
+        }
+    }
+}
---
+++ VB/RIAInstantFeedbackCRUD.Web/NorthwindService.metadata.vb
@@ -0,0 +1,143 @@
+﻿Imports Microsoft.VisualBasic
+	Imports System
+	Imports System.Collections.Generic
+	Imports System.ComponentModel
+	Imports System.ComponentModel.DataAnnotations
+	Imports System.Linq
+	Imports System.ServiceModel.DomainServices.Hosting
+	Imports System.ServiceModel.DomainServices.Server
+Namespace RIAInstantFeedbackCRUD.Web
+
+
+	' The MetadataTypeAttribute identifies CustomersMetadata as the class
+	' that carries additional metadata for the Customers class.
+	<MetadataTypeAttribute(GetType(Customers.CustomersMetadata))> _
+	Partial Public Class Customers
+
+		' This class allows you to attach custom attributes to properties
+		' of the Customers class.
+		'
+		' For example, the following marks the Xyz property as a
+		' required property and specifies the format for valid values:
+		'    [Required]
+		'    [RegularExpression("[A-Z][A-Za-z0-9]*")]
+		'    [StringLength(32)]
+		'    public string Xyz { get; set; }
+		Friend NotInheritable Class CustomersMetadata
+
+			' Metadata classes are not meant to be instantiated.
+			Private Sub New()
+			End Sub
+
+			Private privateAddress As String
+			Public Property Address() As String
+				Get
+					Return privateAddress
+				End Get
+				Set(ByVal value As String)
+					privateAddress = value
+				End Set
+			End Property
+
+			Private privateCity As String
+			Public Property City() As String
+				Get
+					Return privateCity
+				End Get
+				Set(ByVal value As String)
+					privateCity = value
+				End Set
+			End Property
+
+			Private privateCompanyName As String
+			Public Property CompanyName() As String
+				Get
+					Return privateCompanyName
+				End Get
+				Set(ByVal value As String)
+					privateCompanyName = value
+				End Set
+			End Property
+
+			Private privateContactName As String
+			Public Property ContactName() As String
+				Get
+					Return privateContactName
+				End Get
+				Set(ByVal value As String)
+					privateContactName = value
+				End Set
+			End Property
+
+			Private privateContactTitle As String
+			Public Property ContactTitle() As String
+				Get
+					Return privateContactTitle
+				End Get
+				Set(ByVal value As String)
+					privateContactTitle = value
+				End Set
+			End Property
+
+			Private privateCountry As String
+			Public Property Country() As String
+				Get
+					Return privateCountry
+				End Get
+				Set(ByVal value As String)
+					privateCountry = value
+				End Set
+			End Property
+
+			Private privateCustomerID As String
+			Public Property CustomerID() As String
+				Get
+					Return privateCustomerID
+				End Get
+				Set(ByVal value As String)
+					privateCustomerID = value
+				End Set
+			End Property
+
+			Private privateFax As String
+			Public Property Fax() As String
+				Get
+					Return privateFax
+				End Get
+				Set(ByVal value As String)
+					privateFax = value
+				End Set
+			End Property
+
+			Private privatePhone As String
+			Public Property Phone() As String
+				Get
+					Return privatePhone
+				End Get
+				Set(ByVal value As String)
+					privatePhone = value
+				End Set
+			End Property
+
+			Private privatePostalCode As String
+			Public Property PostalCode() As String
+				Get
+					Return privatePostalCode
+				End Get
+				Set(ByVal value As String)
+					privatePostalCode = value
+				End Set
+			End Property
+
+			Private privateRegion As String
+			Public Property Region() As String
+				Get
+					Return privateRegion
+				End Get
+				Set(ByVal value As String)
+					privateRegion = value
+				End Set
+			End Property
+		End Class
+	End Class
+End Namespace
